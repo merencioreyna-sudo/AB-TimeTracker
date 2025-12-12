bbc873c5b3561736e3121331b704410e2a320d08
merencioreyna-sudo <merencioreyna@gmail.com>
2025-12-11 21:52:36 -0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
-// Firebase v10 USANDO SDK CLÁSICO (GARANTIZADO EN GITHUB PAGES)
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBHz_JHDItRfyOI9wx-RhHgFqVNrQeN_ks",
-    authDomain: "ab-timetracker.firebaseapp.com",
-    projectId: "ab-timetracker",
-    storageBucket: "ab-timetracker.appspot.com",
-    messagingSenderId: "1054161107782",
-    appId: "1:1054161107782:web:9f624494525cc969778347"
+  apiKey: "AIzaSyBHz_JHDItRfyOI9wx-RhHgFqVNrQeN_ks",
+  authDomain: "ab-timetracker.firebaseapp.com",
+  projectId: "ab-timetracker",
+  storageBucket: "ab-timetracker.appspot.com",
+  messagingSenderId: "1054161107782",
+  appId: "1:1054161107782:web:9f624494525cc969778347"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 window.auth = firebase.auth();
 
-console.log("FIREBASE JS CARGADO");
-
+console.log("FIREBASE CARGADO OK");
