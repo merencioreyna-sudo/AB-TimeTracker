4ebddb5afd00302e88b03f1873e8e2a8d33a7769
merencioreyna-sudo <merencioreyna@gmail.com>
2026-06-29 12:15:18 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker instalado");
-});
-
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activado");
-});
